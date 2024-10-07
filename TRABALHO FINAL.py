2fbf1e46e52c4730ad470e9f402f7a223ab49555
JOAO2666 <108814202+JOAO2666@users.noreply.github.com>
2024-10-07 12:01:14 -0300

    Delete TRABALHO FINAL.py

--- TRABALHO FINAL.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LivroDetail, LivroList, LivroCreate
-
-urlpatterns = [
-    path("livros/", LivroList.as_view(), name="livro-list"),
-    path("livros/create/", LivroCreate.as_view(), name="livro-create"),
-    path("livros/<int:pk>/", LivroDetail.as_view(), name="livro-detail"),
-]
